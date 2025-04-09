41e7c3697055537447d2c0c87a4603aeda6d4994
afsal cp <cpafsal66@gmail.com>
2025-04-10 03:46:38 +0530

    hosting type

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns=[
     path('',views.Login.as_view(),name='login'),
-    path('signup/',views.Signup.as_view(),name='signup'),
-    path('product/',views.product_data,name='product'),
+    path('signup',views.Signup.as_view(),name='signup'),
+    path('product',views.product_data,name='product'),
     path('cart/add',views.add_to_cart,name='add_to_cart'),
     path('cart/remove',views.remove_from_cart,name='remove_from_cart'),
     path('cart',views.cart,name='cart')
